d867ac189ca4d7558c72a9c003511abadc58356d
VasicNikola98 <vasic.nikola.vasa@gmail.com>
2021-05-01 13:05:38 +0200

    Dodata stranica o nama

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,28 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  const btnScrollTop = document.querySelector("#scrollToTopBtn");
+  const btnMessenger = document.querySelector("#messenger-widget-btn");
+  const btnPhone = document.querySelector("#phone-widget-btn");
 
-    const btnScrollTop = document.querySelector("#scrollToTopBtn");
+  window.addEventListener("scroll", scrollFunction);
 
-    window.addEventListener("scroll", scrollFunction);
-
-    function scrollFunction () {
-        if(window.pageYOffset > 300)
-        {
-            btnScrollTop.style.display = "block";
-            btnScrollTop.addEventListener("click", function() {
-        
-                window.scrollTo({
-                    top: 0,
-                    left: 0,
-                    behavior: "smooth"
-                });
-            });
-        }
-        else
-        {
-            btnScrollTop.style.display = "none";
-        }
+  function scrollFunction() {
+    if (window.pageYOffset > 300) {
+      btnScrollTop.style.display = "block";
+      btnMessenger.style.display = "block";
+      btnPhone.style.display = "block";
+      btnScrollTop.addEventListener("click", function () {
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: "smooth",
+        });
+      });
+    } else {
+      btnScrollTop.style.display = "none";
+      btnPhone.style.display = "none";
+      btnMessenger.style.display = "none";
     }
+  }
 
-    $(".load-wrapper").fadeOut();
-    
+  $(".load-wrapper").fadeOut();
 });
